fa7011eb6c56cbb6fa5d496b033fc57cee99d588
odanylchuk <145114504+O-Danylchuk@users.noreply.github.com>
2024-04-20 15:24:21 +0300

    Add project files.

---
+++ graph/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Graph
+{
+    public abstract class Graph
+    {
+        private int[,] adjacencyMatrix;
+        private List<List<int>> adjacencyList;
+
+        public Graph(int vertices)
+        {
+            adjacencyMatrix = new int[vertices, vertices];
+            adjacencyList = new List<List<int>>();
+            for (int i = 0; i < vertices; i++)
+            {
+                adjacencyList.Add(new List<int>());
+            }
+        }
+
+        public abstract void AddEdge(int startVertex, int endVertex);
+        public abstract void RemoveEdge(int startVertex, int endVertex);
+
+        public abstract void AddVertex();
+        public abstract void RemoveVertex();
+
+    }
+}
